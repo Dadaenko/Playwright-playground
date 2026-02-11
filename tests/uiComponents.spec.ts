d799654009c8fc87915cef2f96af73528f9ce85e
Daria Makarenko <darya.makarenko@gmail.com>
2026-02-11 23:38:20 +0200

    Lists and dropdowns lesson

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -62,4 +62,35 @@ test('Checkboxes', async({page}) => {
     }
 })
 
+test('Lists and Dropdowns', async({page}) =>{
+
+    const dropDownMenu = page.locator('ngx-header nb-select')
+    await dropDownMenu.click()
+
+    page.getByRole('list') //can be used when the list has a UL tag - parent container
+    page.getByRole('listitem') //can be used when the list LI tag - all the list itemsin the array
+
+    //const optionList = page.getByRole('list').locator('nb-option')
+
+    const optionList = page.locator('nb-option-list nb-option')
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+    await optionList.filter({hasText: "Cosmic"}).click()
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+    const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)"
+    }
+    
+    await dropDownMenu.click()
+    for(const color in colors){
+        await optionList.filter({hasText: color}).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if(color != "Corporate")
+            await dropDownMenu.click()
+    }
+})
 
