a032de8f242d0a74e88781bfb9c5df18a5bcadb4
Daria Makarenko <darya.makarenko@gmail.com>
2026-02-13 23:42:02 +0200

    banner update

--- tests/rozetka.spec.ts
+++ tests/rozetka.spec.ts
@@ -4,10 +4,12 @@ test.beforeEach(async ({ page }) => {
     await page.goto('https://rozetka.com.ua/')
 })
 
-test('Click the category in the Catalogue', async({page}) => {
+test('Click the Zoo category in the Catalogue and verify banner', async({page}) => {
 
     const catalogue = await page.getByRole('button', {name: 'Каталог'}).click()
     const category = await page.getByRole('link', { name: 'Зоотовари' }).click()
     expect(category).toBeTruthy
+    const banner = page.locator('#rz-banner-img')
+    expect(banner).toBeVisible
 
 })
