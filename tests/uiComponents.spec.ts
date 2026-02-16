b8837dadce8d66e00c50f9daf8a503f07c8c8b07
Daria Makarenko <darya.makarenko@gmail.com>
2026-02-16 23:17:24 +0200

    Web tables, 2nd part

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -167,3 +167,32 @@ test('Web Tables part 1', async ({page}) =>{
     await page.locator('.nb-checkmark').click()
     await expect(targetRowById.locator('td').nth(5)).toHaveText('testing@for.money.com')
 })
+
+test('Web Tables part 2', async ({page}) => {
+
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    //test filter of the table
+    const ages = ["20", "30", "40", "200"]
+
+    for(let age of ages) {
+        await page.locator('input-filter').getByPlaceholder('Age').clear()
+        await page.locator('input-filter').getByPlaceholder('Age').fill(age)
+        await page.waitForTimeout(500)
+        const ageRows = page.locator('tbody tr')
+
+        for(let row of await ageRows.all()){
+            
+            const cellValue = await row.locator('td').last().textContent()
+
+            if(age == "200"){
+
+                expect(await page.getByRole('table').textContent()).toContain('No data found')
+
+            } else {
+                expect(cellValue).toEqual(age)
+            }
+        }
+    }
+})
