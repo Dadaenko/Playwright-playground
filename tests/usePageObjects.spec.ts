6ec1d4f2cbf3ff945bcf30a215d68fa28e1a5f2f
Daria Makarenko <darya.makarenko@gmail.com>
2026-03-03 22:04:11 +0200

    Parametrized methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 import {NavigationPage} from '../page-object/navigationPage'
+import { formLayoutPage } from '../page-object/formLayoutsPage'
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
@@ -13,5 +14,17 @@ test('Navigate to form page', async ({page}) =>{
     await navigateTo.smartTablePage()
     await navigateTo.toastrPage()
     await navigateTo.tooltipPage()
+
+})
+
+test('Parametrized methods', async ({page}) => {
+    const navigateTo = new NavigationPage(page)
+    const onformLayoutPage = new formLayoutPage(page)
+
+    await navigateTo.formLayoutPage()
+    await onformLayoutPage.submitUsingTheGridFormWithCredentialsAndSelectOption('testing@is.nope', 'dgfhdjslirbvls97468273', 'Option 2')
+
+    await navigateTo.formLayoutPage()
+    await onformLayoutPage.submitInlineFormWithNameEmailAndCheckbox('Samba Di Janeiro', 'amber@sambissimo.com', true)
 
 })
