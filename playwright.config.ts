1a160ca2200daed236458b926d86eb4b0fdf45b2
Daria Makarenko <darya.makarenko@gmail.com>
2026-04-27 20:09:24 +0300

    Use of env variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import type { TestOptions} from './test-options';
 
 /**
  * Read environment variables from file.
@@ -7,11 +8,12 @@ import { defineConfig, devices } from '@playwright/test';
 // import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
+require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
+export default defineConfig<TestOptions>({
 
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -27,7 +29,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:4200/',
+    globalsQaURL: 'https://www.globalsqa.com/demo-site/draganddrop/',
     actionTimeout: 50000,
     navigationTimeout: 50000,
     video: 'on',
